1e409b7f73afc8a626f6fd889fd0f1bdaa8c111c
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-22 22:27:56 -0300

    feat: add list all dbs created and created a new db :sparkles:

--- components/chat/users.vue
+++ components/chat/users.vue
@@ -1,72 +1,96 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { v4 as uuidv4 } from 'uuid';
+import { useDatabaseStore } from '~/stores/databaseStore'
+
+const databaseStore = useDatabaseStore()
+
+const handleWithGetListOfDatabase = () => {
+  databaseStore.getAllDatabaseInIndexedDB()
+}
+
+const handleCreateRandonId = () => {
+  return uuidv4()
+}
+
+const handleWithSetDatabaseId = async (id: string) => {
+  await databaseStore.setDatabaseId(id)
+}
+
+const handleWithCreateANewDatabase = async (id: string) => {
+  await databaseStore.createNewDatabase(id)
+}
+
+onMounted(() => {
+  handleWithSetDatabaseId('test')
+  handleWithGetListOfDatabase()
+})
+</script>
+
 <template>
-  <div class="border-r border-gray-300 min-w-[20rem] w-full sm:w-1/12">
+  <div class="border-r border-gray-300 min-w-[20rem] h-screen w-full md:w-1/3">
     <div class="my-3 mx-3">
       <div class="text-gray-600 relative">
         <span class="flex pl-2 inset-y-0 left-0 absolute items-center">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            viewBox="0 0 24 24" class="h-6 text-gray-300 w-6">
-            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            class="h-6 text-gray-300 w-6"
+          >
+            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
         </span>
-        <input type="search" class="rounded outline-none bg-gray-100 w-full py-2 pl-10 block" name="search"
-          placeholder="Search" required />
+        <input
+          type="search"
+          class="rounded outline-none bg-gray-100 w-full py-2 pl-10 block"
+          name="search"
+          placeholder="Search"
+          required
+        >
       </div>
     </div>
-
-    <ul class="h-[32rem] overflow-auto">
-      <h2 class="my-2 text-lg mb-2 ml-2 text-gray-600">Chats</h2>
-      <li>
+    <div
+      class="border rounded-full border-gray-300 text-sm py-2 mx-5 transition ease-in-out duration-150 items-center hover:bg-gray-100 focus:outline-none text-center"
+    >
+      <button
+        class="flex justify-center items-center text-center w-full h-auto"
+        @click="handleWithCreateANewDatabase(handleCreateRandonId())"
+      >
+        Create a new chat
+      </button>
+    </div>
+    <ul class="h-auto w-full overflow-hidden max-h-screen overflow-y-auto">
+      <h2 class="my-2 text-lg mb-2 ml-2 text-gray-600">
+        Chats
+      </h2>
+      <li
+        v-for="(item, index) in databaseStore.databaseList"
+        :key="index"
+        @click="handleWithSetDatabaseId(item)"
+      >
         <a
-          class="border-b cursor-pointer flex border-gray-300 text-sm py-2 px-3 transition ease-in-out duration-150 items-center hover:bg-gray-100 focus:outline-none">
-          <img class="rounded-full object-cover h-10 w-10"
-            src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg" alt="username" />
+          class="border-b cursor-pointer flex border-gray-300 text-sm py-2 px-3 transition ease-in-out duration-150 items-center hover:bg-gray-100 focus:outline-none"
+        >
+          <img
+            class="rounded-full object-cover h-10 w-10"
+            src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
+            alt="username"
+          >
           <div class="w-full pb-2">
             <div class="flex justify-between">
               <span class="font-semibold ml-2 text-gray-600 block">
-                Jhon Don
+                {{ item }}
               </span>
               <span class="text-sm ml-2 text-gray-600 block">
                 25 minutes
               </span>
             </div>
             <span class="text-sm ml-2 text-gray-600 block">
-              bye
-            </span>
-          </div>
-        </a>
-        <a
-          class="border-b cursor-pointer flex bg-gray-100 border-gray-300 text-sm py-2 px-3 transition ease-in-out duration-150 items-center focus:outline-none">
-          <img class="rounded-full object-cover h-10 w-10"
-            src="https://cdn.pixabay.com/photo/2016/06/15/15/25/loudspeaker-1459128__340.png" alt="username" />
-          <div class="w-full pb-2">
-            <div class="flex justify-between">
-              <span class="font-semibold ml-2 text-gray-600 block">
-                Same
-              </span>
-              <span class="text-sm ml-2 text-gray-600 block">
-                50 minutes
-              </span>
-            </div>
-            <span class="text-sm ml-2 text-gray-600 block">
-              Good night
-            </span>
-          </div>
-        </a>
-        <a
-          class="border-b cursor-pointer flex border-gray-300 text-sm py-2 px-3 transition ease-in-out duration-150 items-center hover:bg-gray-100 focus:outline-none">
-          <img class="rounded-full object-cover h-10 w-10"
-            src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
-          <div class="w-full pb-2">
-            <div class="flex justify-between">
-              <span class="font-semibold ml-2 text-gray-600 block">
-                Emma
-              </span>
-              <span class="text-sm ml-2 text-gray-600 block">
-                6 hour
-              </span>
-            </div>
-            <span class="text-sm ml-2 text-gray-600 block">
-              Good Morning
+              last message
             </span>
           </div>
         </a>
