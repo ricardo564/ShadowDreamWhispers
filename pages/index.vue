2420fe60f55027ca09442352bdf707ec5bc57551
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-22 22:26:07 -0300

    style: add overflow hidden and min height :art:

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
-    <div class="container mx-auto">
-      <div class="border rounded flex min-w-full">
+    <div class="container mx-auto min-h-full w-full h-full">
+      <div class="border rounded min-w-full flex max-h-screen max-w-screen overflow-hidden">
         <ChatUsers />
         <ChatDisplay />
       </div>
