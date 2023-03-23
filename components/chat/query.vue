611815d7942cc04e382df181689539a51fe39d79
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-22 22:28:18 -0300

    feat: add saveQueryOnDatabase  :sparkles:

--- components/chat/query.vue
+++ components/chat/query.vue
@@ -1,35 +1,85 @@
 <script setup lang="ts">
-import Database from '@/services/indexedDb';
+import { v4 as uuidv4 } from 'uuid'
+import { useDatabaseStore } from '~/stores/databaseStore'
 
-const chatId = "12546dddww@#!@"
-const { send } = useChatgpt()
-
-const data = ref('')
-const message = ref('')
-
-async function sendMessage() {
-  const response = await send(message.value)
-  data.value = response
-
-  const item = {
-    id: Date.now().toString(),
-    message: response
-  }
-
-  await Database.saveItem(chatId, item)
+interface Item {
+  id: string
+  message: string
 }
 
+const { send } = useChatgpt()
+
+const handleCreateRandonId = () => {
+  return uuidv4()
+}
+
+const databaseStore = useDatabaseStore()
+
+const message = ref('')
+let loading = false
+const databaseId = databaseStore.getDatabaseId
+const userId = databaseStore.getUserId
+
+const saveQueryOnDatabase = async () => {
+  loading = true
+
+  const query: Item = {
+    id: userId,
+    message: message.value,
+  }
+
+  await databaseStore.saveQuery(databaseId, query)
+    .then(() => {
+      message.value = ''
+      loading = false
+    })
+    .catch((err: any) => {
+      loading = false
+      console.log(err)
+    })
+}
+
+const sendMessage = async () => {
+  loading = true
+
+  await send(message.value)
+    .then((res: any) => {
+      const resWithId: Item = {
+        id: handleCreateRandonId(),
+        message: res,
+      }
+
+      saveQueryOnDatabase()
+
+      databaseStore.saveResponse(databaseId, resWithId)
+      loading = false
+    })
+    .catch((err: any) => {
+      loading = false
+      console.log(err)
+    })
+}
 </script>
+
 <template>
-  <div class="border-t flex border-gray-300 w-full p-3 items-center justify-between">
-    <input v-model="message" type="text" placeholder="Message"
-      class="rounded-full outline-none bg-gray-100 mx-3 w-full py-2 pl-4 block focus:text-gray-700" name="message"
-      required />
-    <button type="submit" @click="sendMessage">
-      <svg class="h-5 transform origin-center text-gray-500 w-5 rotate-90" xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20" fill="currentColor">
+  <div class="border-t flex border-gray-300 w-full h-16 p-3 items-center justify-between">
+    <input v-model="message"
+           type="text"
+           placeholder="Message"
+           class="rounded-full outline-none bg-gray-100 mx-3 w-full py-2 pl-4 block focus:text-gray-700"
+           name="message"
+           :disabled="loading"
+           required
+           @keyup.enter="sendMessage">
+    <button type="submit"
+            :disabled="loading"
+            @click="sendMessage">
+      <svg class="h-5 transform origin-center text-gray-500 w-5 rotate-90"
+           xmlns="http://www.w3.org/2000/svg"
+           viewBox="0 0 20 20"
+           fill="currentColor">
         <path
-          d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+              d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
       </svg>
     </button>
   </div>
