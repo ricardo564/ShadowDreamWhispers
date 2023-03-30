f37ea075bcc20c50611b42c838ade282e360901e
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-29 23:29:19 -0300

    🔥 chore(index.vue): comment out ChatUsers component to remove it from the page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <NuxtLayout name="default">
     <div class="container mx-auto min-h-full w-full h-full grid justify-center align-center">
       <div class="border rounded min-w-full flex max-h-[90vh] max-w-screen overflow-hidden mt-4">
-        <ChatUsers />
+        <!-- <ChatUsers /> -->
         <ChatDisplay />
       </div>
     </div>
