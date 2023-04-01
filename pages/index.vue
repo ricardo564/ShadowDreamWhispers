e24c3cfb8818c51597810f90cc0dd7e6fee9d710
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-31 22:14:03 -0300

    💄 style(index.vue): add background color to container div

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
     <div class="container mx-auto w-full h-screen grid justify-center align-center">
-      <div class="border rounded-3xl min-w-full flex max-h-[99vh] w-screen max-w-[99vw] overflow-hidden my-1 shadow-xl shadow-primary">
+      <div class="border bg-gray-50 rounded-3xl min-w-full flex max-h-[99vh] w-screen max-w-[99vw] overflow-hidden my-1 shadow-xl shadow-primary">
         <!-- <ChatUsers /> -->
         <ChatDisplay />
       </div>
