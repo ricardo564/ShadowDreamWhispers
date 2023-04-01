8f882796ee5d98085e346dfcc7fded171d7d19e1
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-31 21:24:48 -0300

    💄 style(chat): add transition and shadow effects to chat input and button 🎨 style(chat): replace svg icon with bx-icon in chat input button

--- components/chat/query.vue
+++ components/chat/query.vue
@@ -61,12 +61,12 @@ const saveQueryOnDatabase = async function () {
 </script>
 
 <template>
-  <div class="border-t flex border-gray-300 w-full h-16 p-3 items-center justify-between">
+  <div class="border-t flex border-gray-300 w-full h-16 p-3 items-center justify-between transition-all ease-in-out">
     <input
       v-model="message"
       type="text"
       placeholder="Message"
-      class="rounded-full outline-none bg-gray-100 mx-3 w-full py-2 pl-4 block focus:text-gray-700"
+      class="rounded-full outline-none bg-gray-100 mx-3 w-full py-2 pl-4 block focus:text-gray-700 h-14 shadow hover:shadow-xl delay-100"
       name="message"
       required
       autocomplete="off"
@@ -78,16 +78,7 @@ const saveQueryOnDatabase = async function () {
       :disabled="loading"
       @click="saveQueryOnDatabase"
     >
-      <svg
-        class="h-5 transform origin-center text-gray-500 w-5 rotate-90"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
-        />
-      </svg>
+      <i class="bx bx-send bx-lg hover:text-red-800 animate-pop delay-50" />
     </button>
   </div>
 </template>
