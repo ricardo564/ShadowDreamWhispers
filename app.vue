b2722fa50c5029759c3c71e6091436f61070a342
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-31 22:13:01 -0300

    🚀 feat(app.vue): add head metadata to app.vue file

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: 'ShadowDreamWhispers',
+  meta: [
+    { name: 'description', content: 'ShadowDreamWhispers' },
+    { name: 'keywords', content: 'my, app' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  ],
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
