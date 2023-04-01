4418a8bd7049a1cba68e466f22ee053fcd9c417b
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-31 22:29:15 -0300

    🐛 fix(databaseStore.ts): change user name from 'UserName' to 'user' in the initial state

--- stores/databaseStore.ts
+++ stores/databaseStore.ts
@@ -11,7 +11,7 @@ export const useDatabaseStore = defineStore({
     databaseList: [] as string[],
     user: {
       id: 1,
-      name: 'UserName',
+      name: 'user',
     },
   }),
   getters: {
