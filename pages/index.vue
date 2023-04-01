f7900eb74d405f73167ab3faf2cd458949119d97
Ricardo Camilo <39412058+ricardo564@users.noreply.github.com>
2023-03-31 21:24:57 -0300

    💄 style(index.vue): adjust layout and styles of chat display container

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout name="default">
-    <div class="container mx-auto min-h-full w-full h-full grid justify-center align-center">
-      <div class="border rounded min-w-full flex max-h-[90vh] max-w-screen overflow-hidden mt-4">
+    <div class="container mx-auto w-full h-screen grid justify-center align-center">
+      <div class="border rounded-3xl min-w-full flex max-h-[99vh] w-screen max-w-[99vw] overflow-hidden my-1 shadow-xl shadow-primary">
         <!-- <ChatUsers /> -->
         <ChatDisplay />
       </div>
